3ef516273d178153eaada07c22dba44ed281fce8
AumariZJoseph <aumarijoseph@gmail.com>
2025-06-09 21:13:01 -0400

    Update app.py

--- app.py
+++ app.py
@@ -3,6 +3,23 @@ import os
 import shutil
 from ingest import ingest_data
 from query import QASystem
+import m;tl
+
+
+nltk_data_dir = os.path.expanduser('~/.nltk_data')
+os.makedirs(nltk_data_dir, exist_ok=True)
+nltk.data.path.append(nltk_data_dir)
+
+# Try to avoid redownloading
+try:
+    nltk.data.find('tokenizers/punkt')
+except LookupError:
+    nltk.download('punkt', download_dir=nltk_data_dir)
+
+try:
+    nltk.data.find('corpora/stopwords')
+except LookupError:
+    nltk.download('stopwords', download_dir=nltk_data_dir)
 
 # Configuration
 DATA_DIR = "data"
